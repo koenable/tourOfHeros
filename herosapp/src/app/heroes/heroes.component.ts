6869a8b026eb91d8786b0209fa8e290b181c3ed6
Baholo <mokoenabaholo90@gmail.com>
2020-06-15 13:51:44 +0200

    Create a feature component. Used @Input() in secondary component, to link to Primary component.

--- herosapp/src/app/heroes/heroes.component.ts
+++ herosapp/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HEROES } from "../mock-heroes";
+import { Hero } from "../hero";
 
 @Component({
   selector: 'app-heroes',
@@ -9,18 +9,23 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
+
+  //get hero data
   heroes = HEROES;
-
+  
+  //user selection from DOM
   selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //when user clicks on 
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero;
+  }
+
 
 
 }
